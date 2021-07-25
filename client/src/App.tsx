d6ebd51bafcdb0ce2c5d3352030372d869ed7be2
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-07-25 14:51:36 -0400

    finish basic graphql call

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const ENDPOINT = "/graphql";
+
 function App() {
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    const payload = {
+      query: `{
+        hello
+      }`
+    };
+
+    axios.post(ENDPOINT, payload)
+      .then(res => {
+        setResponse(res.data.data.hello);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {response}
     </div>
   );
 }
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,26 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
 
 const app = express();
 const PORT = 8080;
 
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = { hello: () => 'Hello World!' };
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}))
+
 app.get('/', (_req, res) => res.send("Hello World"));
 
 app.listen(PORT, () => {
-    `running on ${PORT}`;
+  `running on ${PORT}`;
 })
